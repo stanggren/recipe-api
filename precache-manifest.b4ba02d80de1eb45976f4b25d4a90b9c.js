e2f0a54a221d4029d1540fce8ee8fa2bf1c299c2
stanggren <astanggren@gmail.com>
2020-11-23 20:02:04 +0100

    Updates

---
+++ precache-manifest.b4ba02d80de1eb45976f4b25d4a90b9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf45762a38ddda2d02bb94e02c2f6709",
+    "url": "/recipe-api/index.html"
+  },
+  {
+    "revision": "8c6aaa9dbb21b6fb4688",
+    "url": "/recipe-api/static/css/main.7e7fff3c.chunk.css"
+  },
+  {
+    "revision": "ea9ed2a944b42b5ab5f2",
+    "url": "/recipe-api/static/js/2.e2da905b.chunk.js"
+  },
+  {
+    "revision": "8c6aaa9dbb21b6fb4688",
+    "url": "/recipe-api/static/js/main.5c23eb85.chunk.js"
+  },
+  {
+    "revision": "ea474f762bb26eeb9ac9",
+    "url": "/recipe-api/static/js/runtime~main.acb4959f.js"
+  }
+]);
